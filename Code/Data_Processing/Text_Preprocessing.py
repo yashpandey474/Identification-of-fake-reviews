3a1eda0bca9b3a964e20033add1364d47be5be5e
Yash Pandey <109035687+yashpandey474@users.noreply.github.com>
2023-06-20 17:13:17 +0530

    Update Text_Preprocessing.py

--- Code/Data_Processing/Text_Preprocessing.py
+++ Code/Data_Processing/Text_Preprocessing.py
@@ -6,9 +6,12 @@ import pandas as pd
 # INITIALISATIONS
 nlp = spacy.load('en_core_web_sm')
 stemmer = PorterStemmer()
+stop_words = nlp.Defaults.stop_words
+
 
 
 # 1. TEXT PROCESSING
+
 
 
 # REMOVE ALL PUNCTUATION
@@ -18,9 +21,6 @@ def separate_punc(doc_text):
 
 
 # REMOVE STOPWORDS - SPACY
-
-stop_words = nlp.Defaults.stop_words
-
 
 def remove_stopwords(text):
     words = text.lower().split()
@@ -38,6 +38,19 @@ def lemmatize_text(text):
     doc = nlp(text)
     return ' '.join([token.lemma_ for token in doc])
 
+#COMBINE THEM INTO ONE PREPROCESSING FUNCTION
+def preprocess_text(text):
+    nlp = spacy.load('en_core_web_sm')
+    stemmer = PorterStemmer()
+    stop_words = nlp.Defaults.stop_words
 
-df['REVIEW_TEXT'][:1000] = df['REVIEW_TEXT'][:1000].apply(
-    lambda d: lemmatize_text(d))
+    words = text.lower().split()
+    return " ".join([token.lemma_ for token in nlp(" ".join([stemmer.stem(word) for word in words if word not in stop_words]))])
+# df['REVIEW_TEXT'][:1000] = df['REVIEW_TEXT'][:1000].apply(
+    #lambda d: lemmatize_text(d))
+    
+# CONVERT THE LABELS TO FAKE & NON FAKE
+# df.loc[df["LABEL"] == "__label1__", "LABEL"] = 'Fake'
+# df.loc[df["LABEL"] == "__label2__", "LABEL"] = 'Not Fake'
+
+
